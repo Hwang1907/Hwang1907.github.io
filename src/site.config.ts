9c50d554d92623bb6b6ba3b25aa368e5a9663bac
Hwang1907 <69113243+Hwang1907@users.noreply.github.com>
2026-02-03 22:15:37 -0500

    initial homework commit

--- src/site.config.ts
+++ src/site.config.ts
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "Hwang1907",
+	title: "Andrew Hwang",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
 	url: "https://Hwang1907.github.io/",
 };
@@ -41,7 +41,7 @@ export const menuLinks: { path: string; title: string }[] = [
 		title: "Blog",
 	},
 	{
-		path: "/notes/",
+		path: "/homework/",
 		title: "Homework",
 	},
 ];
