f2da18ef1a963282be7026c4a5823a9b9694e483
Hwang1907 <69113243+Hwang1907@users.noreply.github.com>
2026-06-08 23:02:04 -0400

    revisions

--- src/site.config.ts
+++ src/site.config.ts
@@ -44,6 +44,10 @@ export const menuLinks: { path: string; title: string }[] = [
 		path: "/photography/",
 		title: "Photography",
 	},
+	{
+		path: "/projects/",
+		title: "Projects",
+	},
 ];
 
 // https://expressive-code.com/reference/configuration/
